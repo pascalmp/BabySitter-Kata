9eee985374076b1002a3b8cdfe955cb638c3ef9b
Pascal Mpawenayo <pm@gmail.com>
2017-01-07 17:15:01 -0500

    Initial Commit for BabySitter-Kata

---
+++ src/test/java/babisitter_kata/BabySitterTest.java
@@ -0,0 +1,11 @@
+package babisitter_kata;
+
+public class BabySitterTest {
+
+	private BabySitter testObject;
+	
+	@Before
+	public void executeThisBeforeEachTest(){
+		testObject = new BabySitter();
+	}
+}
